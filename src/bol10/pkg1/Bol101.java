0e5c974206a21a7f90c5a4ab1f5eb270ba9bf192
Pablite5 <Pablite5@GAMING>
2015-11-23 09:56:21 +0100

    62

---
+++ src/bol10/pkg1/Bol101.java
@@ -0,0 +1,20 @@
+
+package bol10.pkg1;
+
+/**
+ *
+ * @author Pablite5
+ */
+public class Bol101 {
+
+    
+    public static void main(String[] args) {
+        Calculo obx=new Calculo();
+        obx.SetAdivinar();
+        obx.ejecutarJuego();
+        
+    }
+    
+}
+
+
